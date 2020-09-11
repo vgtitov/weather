9cda7cb8dbdb822e045ba686e22b420da819f484
vgtitov <v.titov.7@gmail.com>
2020-09-11 17:45:35 +0300

    Initial Commit

---
+++ weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  weather
+//
+//  Created by it10 on 11.09.2020.
+//  Copyright © 2020 Viachaslau Tsitou. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
